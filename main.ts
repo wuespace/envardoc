c46b7d23b8448353bd9909e98f353a68f48a1dea
Zuri Klaschka <pklaschka@users.noreply.github.com>
2024-12-08 18:29:45 +0100

    Enable file output modes for `docs` and `example`

--- main.ts
+++ main.ts
@@ -9,33 +9,47 @@ if (import.meta.main) {
   const docs = new Command()
     .arguments("<path:file>")
     .description("Generate documentation from .env files")
-    .action(async (_options, path: string) => {
-      console.log(
-        await parseAndConvert(
-          path,
-          new DocsWriter(),
-        ),
+    .option("-o, --output <path:string>", "Output file path")
+    .action(async ({ output }, path) => {
+      const markdown = await parseAndConvert(
+        path,
+        new DocsWriter(),
       );
+      if (output) {
+        await Deno.writeTextFile(output, markdown);
+        console.log(`File written: ${output}`);
+      } else {
+        console.log(markdown);
+      }
     });
 
   const envExample = new Command()
     .arguments("<path:string>")
     .description("Generate example .env file")
-    .action(async (_options, path: string) => {
-      console.log(
-        await parseAndConvert(
-          path,
-          new EnvExampleWriter(),
-        ),
+    .option("-r, --replace", "Replace existing .env file")
+    .option("-o, --output <path:string>", "Output file path")
+    .action(async ({ replace, output }, path: string) => {
+      const fileContents = await parseAndConvert(
+        path,
+        new EnvExampleWriter(),
       );
+      if (replace || output) {
+        await Deno.writeTextFile(output ?? path, fileContents);
+        console.log(`File written: ${path}`);
+      } else {
+        console.log(fileContents);
+      }
     });
 
   await new Command()
     .name("envardoc")
     .version(metadata.version)
     .description(metadata.description)
-    .meta('Deno', Deno.version.deno)
-    .meta('FS Read Permissions', Deno.permissions.querySync({ name: "read" }).state)
+    .meta("Deno", Deno.version.deno)
+    .meta(
+      "FS Read Permissions",
+      Deno.permissions.querySync({ name: "read" }).state,
+    )
     .command("docs", docs)
     .command("example", envExample)
     .parse(Deno.args);
